5c9e755753a48e2825ecd81eff5013b726db96ee
Gino Filicetti <ginof@google.com>
2022-10-20 17:33:13 -0400

    added a variables section that we'll use in each script

--- scripts/task-1.sh
+++ scripts/task-1.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
+
+# variables
+PROJECT_ID=$(gcloud config get-value project)
+CLUSTER_NAME=m4a-processing
+ZONE=us-central1-a
+
 # create a single node cluster for processing migration
-gcloud container clusters create m4a-processing \
+gcloud container clusters create $CLUSTER_NAME \
     --num-nodes=1 \
-    --zone=us-central1-a
+    --zone=$ZONE
