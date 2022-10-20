11eee8ebd684e29995b23badc75b7514c864808d
Gino Filicetti <ginof@google.com>
2022-10-20 17:33:25 -0400

    first crack at the full task 2

--- scripts/task-2.sh
+++ scripts/task-2.sh
@@ -1,13 +1,29 @@
 #! /bin/bash
 
+# variables
 PROJECT_ID=$(gcloud config get-value project)
-printf '${PROJECT_ID}\n'
-exit 0
+CLUSTER_NAME=m4a-processing
+ZONE=us-central1-a
 
 # create a service account for migrate
 gcloud iam service-accounts create m4a-install
 
 # give the service account storage admin rights
-gcloud projects add-iam-policy-binding ${PROJECT_ID}  \
+gcloud projects add-iam-policy-binding $PROJECT_ID  \
   --member="serviceAccount:m4a-install@${PROJECT_ID}.iam.gserviceaccount.com" \
   --role="roles/storage.admin"
+
+# download key file for this service account
+gcloud iam service-accounts keys create m4a-install.json \
+  --iam-account=m4a-install@$PROJECT_ID.iam.gserviceaccount.com
+
+# connect to the cluster
+gcloud container clusters get-credentials $CLUSTER_NAME \
+  --zone $ZONE
+
+# set up migrate components on our cluster
+migctl setup install --json-key=m4a-install.json
+
+# confirm setup
+printf 'Running migctl doctor to confirm deployment\n'
+migctl doctor
