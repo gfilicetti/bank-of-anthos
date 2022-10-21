bb2035e6724d603420c0c4d490b25e133d8867a6
Gino Filicetti <ginof@google.com>
2022-10-21 14:35:37 -0400

    fixed the source name

--- scripts/task-4.sh
+++ scripts/task-4.sh
@@ -5,9 +5,9 @@ PROJECT_ID=$(gcloud config get-value project)
 CLUSTER_NAME=m4a-processing
 ZONE=us-central1-a
 SOURCE_NAME=m4a-source 
-JOB_NAME=ledgermonolith-migration
 CE_SRC=m4a-ce-src
+JOB_NAME=ledgermonolith-migration
 VM_NAME=ledgermonolith-service
 
 # run a migration job that we just set up
-migctl migration create $JOB_NAME --source $CE_SRC --vm-id $VM_NAME
+migctl migration create $JOB_NAME --source $SOURCE_NAME --vm-id $VM_NAME
