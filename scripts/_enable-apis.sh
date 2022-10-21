94fe8c070d47fd5dd771ea4a7b6fa937145b536a
Gino Filicetti <ginof@google.com>
2022-10-21 19:13:30 -0400

    enabling monitoring

--- scripts/_enable-apis.sh
+++ scripts/_enable-apis.sh
@@ -3,3 +3,4 @@
 gcloud services enable "compute.googleapis.com"
 gcloud services enable "container.googleapis.com"
 gcloud services enable "cloudresourcemanager.googleapis.com"
+gcloud services enable "monitoring.googleapis.com"
