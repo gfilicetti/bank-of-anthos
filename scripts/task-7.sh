36700a52ff360e79c1109e687c7c5e26da6f907a
Gino Filicetti <ginof@google.com>
2022-10-21 19:34:16 -0400

    sorted out how to download artifacts and deploy to the prod cluster

--- scripts/task-7.sh
+++ scripts/task-7.sh
@@ -2,16 +2,25 @@
 
 # variables
 PROJECT_ID=$(gcloud config get-value project)
+CLUSTER_NAME=m4a-processing
 ZONE=us-central1-a
 SOURCE_NAME=m4a-source 
 CE_SRC=m4a-ce-src
 JOB_NAME=ledgermonolith-migration
 VM_NAME=ledgermonolith-service
-CLUSTER_NAME=cymbal-monolith-cluster
+PROD_CLUSTER_NAME=cymbal-monolith-cluster
+ARTIFACT_DIR=artifacts
 
-# get credential for the new cluster
+# get credential for the processing cluster so we can use migctl
 gcloud container clusters get-credentials $CLUSTER_NAME \
   --project=$PROJECT_ID --zone=$ZONE
 
-# deploy new container to our prod cluster
-xxx
+# download the artifacts to disk
+migctl migration get-artifacts $JOB_NAME -d $ARTIFACT_DIR
+
+# switch to our prod cluster now
+gcloud container clusters get-credentials $PROD_CLUSTER_NAME \
+  --project=$PROJECT_ID --zone=$ZONE
+
+# kubectl apply that to the current cluster
+kubectl apply -f artifacts/deployment_spec.yaml
