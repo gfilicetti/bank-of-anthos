fabafc82c31d1455b69510ce2c73469d2283b6e3
Gino Filicetti <ginof@google.com>
2022-10-21 14:44:07 -0400

    first check in for script to edit migration plan

---
+++ scripts/task-5.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# variables
+PROJECT_ID=$(gcloud config get-value project)
+CLUSTER_NAME=m4a-processing
+ZONE=us-central1-a
+SOURCE_NAME=m4a-source 
+CE_SRC=m4a-ce-src
+JOB_NAME=ledgermonolith-migration
+VM_NAME=ledgermonolith-service
+
+# download the migration plan
+migctl migration get $JOB_NAME
+exit 0
+
+# edit $JOB_NAME.yaml and add in
+# dataVolumes:
+#   - folders:
+#     - /var/lib/postgresql
+printf 'edit yaml here...\n'
+
+# update the revised plan
+migctl migration update $JOB_NAME --file $JOB_NAME.yaml
