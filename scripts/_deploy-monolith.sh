9264169b353265f74ca68f94727eef8b2e037f21
Gino Filicetti <ginof@google.com>
2022-10-21 13:43:04 -0400

    exporting variable so that they are seen by the deploy script.

--- scripts/_deploy-monolith.sh
+++ scripts/_deploy-monolith.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # variables
-PROJECT_ID=$(gcloud config get-value project)
-ZONE=us-central1-a
-GCS_BUCKET=bank-of-anthos-ci
+export PROJECT_ID=$(gcloud config get-value project)
+export ZONE=us-central1-a
+export GCS_BUCKET=bank-of-anthos-ci
 
 # run deploy script
 # since we are using the canonical bucket, all the build artifacts will already be there.
