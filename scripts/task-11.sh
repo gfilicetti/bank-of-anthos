66fc78114e637b77c36c3b3a782d79d777207d0e
Gino Filicetti <ginof@google.com>
2023-05-25 14:38:43 -0400

    added trigger creation to task 11 Added deployment and restart to cloud deploy

--- scripts/task-11.sh
+++ scripts/task-11.sh
@@ -5,16 +5,32 @@ PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUM=$(gcloud projects describe ${PROJECT_ID} --format json | jq -r .projectNumber)
 ZONE=us-central1-a
 CLOUD_BUILD_SA="serviceAccount:${PROJECT_NUM}@cloudbuild.gserviceaccount.com"
+TRIGGER_NAME="deploy-cymbal-k8s"
+GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
+GSR_REPO_NAME=cymbal-bank-repo
+
+GSR_REPO_URL=ssh://admin@ginof.altostrat.com@source.developers.google.com:2022/p/l300-appmod-lab/r/cymbal-bank-repo
+GSR_REMOTE_NAME=gsr
+GITHUB_REPO_URL=https://github.com/GoogleCloudPlatform/bank-of-anthos.git
+GITHUB_REPO_DIR=cymbal-bank-github
 
 # give cloud build SA permissions to deploy to GKE
 gcloud projects add-iam-policy-binding $PROJECT_ID \
   --member="${CLOUD_BUILD_SA}" \
   --role="roles/container.admin"
 
+# NOTE !!!!!!!!!!!!!!!!!!!!
+# Make sure that cloud-build.yaml is in the root of the GSR repo or this trigger won't find it
+# --------------------------
+
+# set up the cloud build trigger to run on everything that gets checked into main
+gcloud builds triggers create cloud-source-repositories --name=$TRIGGER_NAME \
+  --build-config="cloud-build.yaml" \
+  --repo=$GSR_REPO_NAME \
+  --branch-pattern="^main$"
 
 
 
-# GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
 # GKE_PROD_NODES=4
 # CHANNEL=stable
 # CP_VERSION=1.23
