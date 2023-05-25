3ee6628d815177dcfed03aa4feafae9c04f3a5b1
Gino Filicetti <ginof@google.com>
2023-05-25 13:43:33 -0400

    testing getting the SA

--- scripts/task-11.sh
+++ scripts/task-11.sh
@@ -4,6 +4,7 @@
 PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUM=$(gcloud projects describe ${PROJECT_ID} --format json | jq -r .projectNumber)
 ZONE=us-central1-a
+CLOUD_BUILD_SA='${PROJECT_NUM}@cloudbuild.gserviceaccount.com'
 GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
 GKE_PROD_NODES=4
 CHANNEL=stable
@@ -16,7 +17,7 @@ GSR_REMOTE_NAME=gsr
 GITHUB_REPO_URL=https://github.com/GoogleCloudPlatform/bank-of-anthos.git
 GITHUB_REPO_DIR=cymbal-bank-github
 
-echo $PROJECT_NUM
+echo $CLOUD_BUILD_SA
 
 
 
