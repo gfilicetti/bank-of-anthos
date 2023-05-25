805dd80722217083289e082fc312c357198c0d6d
Gino Filicetti <ginof@google.com>
2023-05-25 13:53:55 -0400

    added permission granting

--- scripts/task-11.sh
+++ scripts/task-11.sh
@@ -4,25 +4,27 @@
 PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUM=$(gcloud projects describe ${PROJECT_ID} --format json | jq -r .projectNumber)
 ZONE=us-central1-a
-CLOUD_BUILD_SA="${PROJECT_NUM}@cloudbuild.gserviceaccount.com"
-GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
-GKE_PROD_NODES=4
-CHANNEL=stable
-CP_VERSION=1.23
-NODE_VM_TYPE=e2-standard-4
+CLOUD_BUILD_SA="serviceAccount:${PROJECT_NUM}@cloudbuild.gserviceaccount.com"
 
-GSR_REPO_NAME=cymbal-bank-repo
-GSR_REPO_URL=ssh://admin@ginof.altostrat.com@source.developers.google.com:2022/p/l300-appmod-lab/r/cymbal-bank-repo
-GSR_REMOTE_NAME=gsr
-GITHUB_REPO_URL=https://github.com/GoogleCloudPlatform/bank-of-anthos.git
-GITHUB_REPO_DIR=cymbal-bank-github
-
-echo $CLOUD_BUILD_SA
+# give cloud build SA permissions to deploy to GKE
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="${CLOUD_BUILD_SA}" \
+  --role="roles/container.admin"
 
 
 
 
+# GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
+# GKE_PROD_NODES=4
+# CHANNEL=stable
+# CP_VERSION=1.23
+# NODE_VM_TYPE=e2-standard-4
 
+# GSR_REPO_NAME=cymbal-bank-repo
+# GSR_REPO_URL=ssh://admin@ginof.altostrat.com@source.developers.google.com:2022/p/l300-appmod-lab/r/cymbal-bank-repo
+# GSR_REMOTE_NAME=gsr
+# GITHUB_REPO_URL=https://github.com/GoogleCloudPlatform/bank-of-anthos.git
+# GITHUB_REPO_DIR=cymbal-bank-github
 
 # # create a new GKE cluster according to specs
 # gcloud container clusters create $GKE_PROD_CLUSTER_NAME \
