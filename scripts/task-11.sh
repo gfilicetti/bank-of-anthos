c6e3e49c02bca5db37ae66092b9cb5b3567d57b6
Gino Filicetti <ginof@google.com>
2023-05-25 15:19:47 -0400

    Clean up task 11 Create initial script for task 12

--- scripts/task-11.sh
+++ scripts/task-11.sh
@@ -5,14 +5,9 @@ PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUM=$(gcloud projects describe ${PROJECT_ID} --format json | jq -r .projectNumber)
 ZONE=us-central1-a
 CLOUD_BUILD_SA="serviceAccount:${PROJECT_NUM}@cloudbuild.gserviceaccount.com"
-TRIGGER_NAME="deploy-cymbal-k8s"
 GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
 GSR_REPO_NAME=cymbal-bank-repo
-
-GSR_REPO_URL=ssh://admin@ginof.altostrat.com@source.developers.google.com:2022/p/l300-appmod-lab/r/cymbal-bank-repo
-GSR_REMOTE_NAME=gsr
-GITHUB_REPO_URL=https://github.com/GoogleCloudPlatform/bank-of-anthos.git
-GITHUB_REPO_DIR=cymbal-bank-github
+TRIGGER_NAME="deploy-cymbal-k8s"
 
 # give cloud build SA permissions to deploy to GKE
 gcloud projects add-iam-policy-binding $PROJECT_ID \
@@ -32,41 +27,3 @@ gcloud builds triggers create cloud-source-repositories --name=$TRIGGER_NAME \
 # NOTE !!!!!!!!!!!!!!!!!!!!
 # Make sure that the v1 version of frontend.yaml is in the GSR or it will fail
 # --------------------------
-
-
-
-# GKE_PROD_NODES=4
-# CHANNEL=stable
-# CP_VERSION=1.23
-# NODE_VM_TYPE=e2-standard-4
-
-# GSR_REPO_NAME=cymbal-bank-repo
-# GSR_REPO_URL=ssh://admin@ginof.altostrat.com@source.developers.google.com:2022/p/l300-appmod-lab/r/cymbal-bank-repo
-# GSR_REMOTE_NAME=gsr
-# GITHUB_REPO_URL=https://github.com/GoogleCloudPlatform/bank-of-anthos.git
-# GITHUB_REPO_DIR=cymbal-bank-github
-
-# # create a new GKE cluster according to specs
-# gcloud container clusters create $GKE_PROD_CLUSTER_NAME \
-#   --project=$PROJECT_ID --zone=$ZONE \
-#   --machine-type=$NODE_VM_TYPE --num-nodes=$GKE_PROD_NODES \
-#   --release-channel=$CHANNEL --cluster-version=$CP_VERSION \
-#   --monitoring=SYSTEM --logging=SYSTEM,WORKLOAD --subnetwork=default \
-#   --tags=$GKE_PROD_CLUSTER_NAME --labels csm=
-
-# # get kubectl context for this cluster
-# gcloud container clusters get-credentials $GKE_PROD_CLUSTER_NAME \
-#   --project=$PROJECT_ID --zone=$ZONE
-
-# # move in the manually created v1 of the deployment
-# mv ../kubernetes-manifests/frontend.yaml kubernetes-manifests/frontend.orig.yaml 
-# cp ../kubernetes-manifests/frontend.v1.yaml kubernetes-manifests/frontend.yaml 
-
-# # deploy the application to the cluster
-# kubectl apply -f ../extras/jwt/jwt-secret.yaml
-# kubectl apply -f ../kubernetes-manifests
-
-# # NOTE !!!!!!!!!!!!
-# # Run 'kubectl get pods' and make sure they're all started
-# # Then run "kubectl get service frontend | awk '{print $4}'" to get the IP address
-# # ------------------
---
+++ scripts/task-12.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# variables
+PROJECT_ID=$(gcloud config get-value project)
+ZONE=us-central1-a
+GKE_DEV_CLUSTER_NAME=cymbal-bank-dev
+GKE_DEV_NODES=4
+CHANNEL=stable
+CP_VERSION=1.23
+NODE_VM_TYPE=e2-standard-4
+
+# create the DEV cluster
+gcloud container clusters create $GKE_DEV_CLUSTER_NAME \
+  --project=$PROJECT_ID --zone=$ZONE \
+  --machine-type=$NODE_VM_TYPE --num-nodes=$GKE_DEV_NODES \
+  --release-channel=$CHANNEL --cluster-version=$CP_VERSION \
+  --monitoring=SYSTEM --logging=SYSTEM,WORKLOAD --subnetwork=default \
+  --tags=$GKE_DEV_CLUSTER_NAME --labels csm=
+
+# get kubectl context for this cluster
+gcloud container clusters get-credentials $GKE_DEV_CLUSTER_NAME \
+  --project=$PROJECT_ID --zone=$ZONE
+
+# move in the manually created v1 of the deployment
+mv ../kubernetes-manifests/frontend.yaml ../kubernetes-manifests/frontend.yaml.orig
+cp ../kubernetes-manifests/frontend.yaml.v1 ../kubernetes-manifests/frontend.yaml 
+
+# deploy the application to the cluster
+kubectl apply -f ../extras/jwt/jwt-secret.yaml
+kubectl apply -f ../kubernetes-manifests
+
+# NOTE !!!!!!!!!!!!
+# Run 'kubectl get pods' and make sure they're all started
+# Then run "kubectl get service frontend | awk '{print $4}'" to get the IP address
+# ------------------
