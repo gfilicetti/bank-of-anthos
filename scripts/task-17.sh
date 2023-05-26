227c5ecb9617a6dc0cd68f1248bd02180af24373
Gino Filicetti <ginof@google.com>
2023-05-25 23:02:16 -0400

    added the actual fw rule creation

--- scripts/task-17.sh
+++ scripts/task-17.sh
@@ -6,17 +6,26 @@ ZONE=us-central1-a
 GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
 GKE_DEV_CLUSTER_NAME=cymbal-bank-dev
 NAMESPACE=default
+NETWORK=default
+FW_RULE_NAME=allow-istio
+
+# needed when getting CIDRS
+function join_by { local IFS="$1"; shift; echo "$*"; }
 
 # get kubectl context for this cluster
 gcloud container clusters get-credentials $GKE_PROD_CLUSTER_NAME \
   --project=$PROJECT_ID --zone=$ZONE
 
-function join_by { local IFS="$1"; shift; echo "$*"; }
-
+# Get CIDRS and NETTAGS for all clusters in our project, we're going to allow connectivity between them all
 ALL_CLUSTER_CIDRS=$(gcloud container clusters list --project $PROJECT_ID --format='value(clusterIpv4Cidr)' | sort | uniq)
 ALL_CLUSTER_CIDRS=$(join_by , $(echo "${ALL_CLUSTER_CIDRS}"))
 ALL_CLUSTER_NETTAGS=$(gcloud compute instances list --project $PROJECT_ID --format='value(tags.items.[0])' | sort | uniq)
 ALL_CLUSTER_NETTAGS=$(join_by , $(echo "${ALL_CLUSTER_NETTAGS}"))
 
-printf "$ALL_CLUSTER_CIDRS\n"
-printf "$ALL_CLUSTER_NETTAGS\n"
+gcloud compute firewall-rules create $FW_RULE_NAME \
+    --allow=tcp,udp,icmp,esp,ah,sctp \
+    --direction=INGRESS \
+    --priority=900 \
+    --source-ranges="${ALL_CLUSTER_CIDRS}" \
+    --target-tags="${ALL_CLUSTER_NETTAGS}" \
+    --network=$NETWORK
