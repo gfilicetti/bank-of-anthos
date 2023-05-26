43f62013a1dc9b2f06e5c597f873cec65d99005f
Gino Filicetti <ginof@google.com>
2023-05-25 23:36:40 -0400

    added secrets creation

--- scripts/task-18.sh
+++ scripts/task-18.sh
@@ -6,8 +6,8 @@ ZONE=us-central1-a
 GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
 GKE_DEV_CLUSTER_NAME=cymbal-bank-dev
 CONFIG_MAP_NAME=asm-options
-CONTEXT_PROD="gke_l300-appmod-lab_us-central1-a_cymbal-bank-prod"
-CONTEXT_DEV="gke_l300-appmod-lab_us-central1-a_cymbal-bank-dev"
+CONTEXT_PROD=gke_l300-appmod-lab_us-central1-a_cymbal-bank-prod
+CONTEXT_DEV=gke_l300-appmod-lab_us-central1-a_cymbal-bank-dev
 
 # NOTE !!!!!!!!!!!!!!!!!!!!!!
 # Use this command to list out all the contexts in your kube config
@@ -17,16 +17,20 @@ CONTEXT_DEV="gke_l300-appmod-lab_us-central1-a_cymbal-bank-dev"
 # needed when getting CIDRS
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-# get kubectl context for prod
-# gcloud container clusters get-credentials $GKE_PROD_CLUSTER_NAME \
-#   --project=$PROJECT_ID --zone=$ZONE
-
-# create the configmap for multicluster
+# create the configmap for multicluster on prod
 kubectl --context=$CONTEXT_PROD create configmap $CONFIG_MAP_NAME -n istio-system --from-file <(echo '{"data":{"multicluster_mode":"connected"}}')
 
-# get kubectl context for dev
-# gcloud container clusters get-credentials $GKE_DEV_CLUSTER_NAME \
-#   --project=$PROJECT_ID --zone=$ZONE
-
-# create the configmap for multicluster
+# create the configmap for multicluster on dev
 kubectl --context=$CONTEXT_DEV create configmap $CONFIG_MAP_NAME -n istio-system --from-file <(echo '{"data":{"multicluster_mode":"connected"}}')
+
+# create remote secret for prod
+PRIV_IP_PROD=`gcloud container clusters describe "${GKE_PROD_CLUSTER_NAME}" \
+  --zone "${ZONE}" --format "value(privateClusterConfig.privateEndpoint)"`
+
+../asm/istioctl x create-remote-secret --context=${CONTEXT_PROD} --name=${GKE_PROD_CLUSTER_NAME} --server=https://${PRIV_IP_PROD} > ${CONTEXT_PROD}.secret
+
+# create remote secret for dev
+PRIV_IP_DEV=`gcloud container clusters describe "${GKE_DEV_CLUSTER_NAME}" \
+  --zone "${ZONE}" --format "value(privateClusterConfig.privateEndpoint)"`
+
+../asm/istioctl x create-remote-secret --context=${CONTEXT_DEV} --name=${GKE_DEV_CLUSTER_NAME} --server=https://${PRIV_IP_DEV} > ${CONTEXT_DEV}.secret
