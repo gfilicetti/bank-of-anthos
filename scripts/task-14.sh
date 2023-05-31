4b9f49e4db43b8719edbac316245434ede77e06f
Gino Filicetti <ginof@google.com>
2023-05-31 19:41:23 -0400

    Added the extra step of downloading the asmcli command from the web

--- scripts/task-14.sh
+++ scripts/task-14.sh
@@ -5,9 +5,15 @@ PROJECT_ID=$(gcloud config get-value project)
 ZONE=us-central1-a
 GKE_PROD_CLUSTER_NAME=cymbal-bank-prod
 OUTPUT_DIR=../asm
+ASMCLI_INSTALL_URL=https://storage.googleapis.com/csm-artifacts/asm/asmcli_1.16 
+ASMCLI_CMD=asmcli
+
+# download asmcli fresh to use it
+curl $ASMCLI_INSTALL_URL > $ASMCLI_CMD
+chmod 755 $ASMCLI_CMD
 
 # install ASM on the production cluster
-asmcli install \
+$ASMCLI_CMD install \
   --project_id $PROJECT_ID \
   --cluster_name $GKE_PROD_CLUSTER_NAME \
   --cluster_location $ZONE \
